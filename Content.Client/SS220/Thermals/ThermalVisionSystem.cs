2800c499fd814b79e2a7fc7c6c81964f18abe0d9
Dexler <69513582+DexlerXD@users.noreply.github.com>
2024-07-03 16:02:17 +0300

    Update Content.Client/SS220/Thermals/ThermalVisionSystem.cs
    Co-authored-by: Anri <elyasin.ane@mail.ru>

--- Content.Client/SS220/Thermals/ThermalVisionSystem.cs
+++ Content.Client/SS220/Thermals/ThermalVisionSystem.cs
@@ -1,4 +1,4 @@
-// Original code github.com/CM-14 Licence MIT, EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+// Original code github.com/CM-14 Licence MIT, All edits under © SS220, EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using Content.Shared.SS220.Thermals;
 using Robust.Client.Graphics;
 using Robust.Client.Player;
